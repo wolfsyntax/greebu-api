254087ab9e53439372f0c0575a2d17b01fc04aeb
Wolf Syntax <jaysonalpe@gmail.com>
2023-06-27 21:43:54 +0800

    Initialize module for posting

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,47 +24,54 @@ class PostController extends Controller
     public function store(Request $request)
     {
         //
-        $request->validate([
-            'content'           => ['sometime', 'bail', 'required', 'string'],
+        // $request->validate([
+        //     'content'           => ['sometime', 'bail', 'required', 'string'],
+        //     'attachment_type'   => ['required', 'in:image,video,audio,none', 'bail',],
+        //     'attachment'        => ['sometimes', 'required_unless:attachment_type,none',],
+        //     'latitude'          => ['sometimes', 'required_unless:longitude,null', 'regex:/^[-]?(([0-8]?[0-9])\.(\d+))|(90(\.0+)?)$/'],
+        //     'longitude'         => ['sometimes', 'required_unless:latitude,null', 'regex:/^[-]?((((1[0-7][0-9])|([0-9]?[0-9]))\.(\d+))|180(\.0+)?)$/'],
+        // ]);
+        $validator = Validator::make($request->all(), [
+            'content'           => ['bail', 'required_if:attachment,null', 'string'],
             'attachment_type'   => ['required', 'in:image,video,audio,none', 'bail',],
             'attachment'        => ['sometimes', 'required_unless:attachment_type,none',],
             'latitude'          => ['sometimes', 'required_unless:longitude,null', 'regex:/^[-]?(([0-8]?[0-9])\.(\d+))|(90(\.0+)?)$/'],
             'longitude'         => ['sometimes', 'required_unless:latitude,null', 'regex:/^[-]?((((1[0-7][0-9])|([0-9]?[0-9]))\.(\d+))|180(\.0+)?)$/'],
         ]);
-        // $validator = Validator::make($request->all(), [
-        //     'artist_type'       => ['required', 'exists:artist_types,title',],
-        //     'artist_name'       => ['required', 'string',],
-        //     'genre'             => ['required', 'array',],
-        //     'bio'               => ['sometimes', 'required', 'string',],
-        //     'avatar'            => ['required', 'image', 'mimes:svg,webp,jpeg,jpg,png,bmp',],
-        //     'street'            => ['required', 'string',],
-        //     'city'              => ['required', 'string',],
-        //     'province'          => ['required', 'string',],
-        // ]);
 
-        // if ($validator->fails()) {
-        //     return response()->json([
-        //         'status' => 422,
-        //         'message' => "Invalid data",
-        //         'result' => [
-        //             'errors' => $validator->errors(),
-        //         ],
-        //     ], 203);
-        // }
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => "Invalid data",
+                'result' => [
+                    'errors' => $validator->errors(),
+                ],
+            ], 203);
+        }
 
         $profile = Profile::with('roles')->where('user_id', auth()->user()->id)->first();
 
-        $nprof = [
-            'business_name'     => $request->input('artist_name'),
-            'bio'               => $request->input('bio', '123'),
-            'street_address'    => $request->input('street'),
-            'city'              => $request->input('city'),
-            'province'          => $request->input('province'),
-        ];
+        $post = Post::create([
+            'creator_id',
+            'attachment_type'   => $request->input('attachment_type', 'none'),
+            'attachment'        => $request->input('attachment'),
+            'content'           => $request->input('content'),
+            'longitude'         => $request->input('longitude'),
+            'latitude'          => $request->input('latitude'),
+            'is_schedule'       => $request->input('is_schedule', false),
+            'scheduled_at'      => $request->input('schedule_at', now()),
+        ]);
+        // $nprof = [
+        //     'business_name'     => $request->input('artist_name'),
+        //     'bio'               => $request->input('bio', '123'),
+        //     'street_address'    => $request->input('street'),
+        //     'city'              => $request->input('city'),
+        //     'province'          => $request->input('province'),
+        // ];
 
-        if ($request->hasFile('avatar') && $request->file('avatar')->isValid()) {
-            $nprof['avatar'] = $request->file('avatar')->store('image', 'public');;
-        }
+        // if ($request->hasFile('avatar') && $request->file('avatar')->isValid()) {
+        //     $nprof['avatar'] = $request->file('avatar')->store('image', 'public');;
+        // }
 
 
         return response()->json([
