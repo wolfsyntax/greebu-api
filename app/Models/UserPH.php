60cbf7dfe42aad7a626f10a6db14339c145e339c
Wolf Syntax <jaysonalpe@gmail.com>
2023-07-19 20:20:47 +0800

    Phone number seeder PH specific

---
+++ app/Models/UserPH.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class UserPH extends Model
+{
+    use HasFactory, SoftDeletes, HasUuids;
+
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'username',
+        'email',
+        'phone',
+        'password',
+        'last_login',
+        'google_id', 'facebook_id', 'email_verified_at',
+        'remember_token',
+        'last_login',
+    ];
+
+    protected $appends = [
+        'fullname',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'id' => 'string',
+        'last_login' => 'datetime',
+    ];
+
+    /**
+     * Get Full name
+     * @return string
+     */
+    public function getFullnameAttribute(): string
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+    public function setPasswordAttribute(string $value): string
+    {
+        return $this->attributes['password'] = hash('sha256', $value, false);
+    }
+}
