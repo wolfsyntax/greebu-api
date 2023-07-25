b2dbdfd32d569fc861eab1afb4b9bb3039dbb4e8
Wolf Syntax <jaysonalpe@gmail.com>
2023-07-25 14:00:27 +0800

    Add text to button

--- app/Notifications/ForgotPass.php
+++ app/Notifications/ForgotPass.php
@@ -11,56 +11,56 @@ use Illuminate\Support\HtmlString;
 
 class ForgotPass extends Notification
 {
-  use Queueable;
+    use Queueable;
 
-  protected $token;
-  protected $url;
-  protected $user;
-  /**
-   * Create a new notification instance.
-   */
-  public function __construct($token, $user)
-  {
-    //
-    $this->url = env('FRONTEND_URL', 'http://localhost:5173') . '/password/reset/' . $token;
-    $this->user = $user;
-  }
+    protected $token;
+    protected $url;
+    protected $user;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($token, $user)
+    {
+        //
+        $this->url = env('FRONTEND_URL', 'http://localhost:5173') . '/password/reset/' . $token;
+        $this->user = $user;
+    }
 
-  /**
-   * Get the notification's delivery channels.
-   *
-   * @return array<int, string>
-   */
-  public function via(object $notifiable): array
-  {
-    return ['mail'];
-  }
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
 
-  /**
-   * Get the mail representation of the notification.
-   */
-  public function toMail(object $notifiable): MailMessage
-  {
-    $salutation = Lang::get("Thank You,<br/>Geebu Support");
-    return (new MailMessage)
-      ->subject(Lang::get('Reset Password Notification'))
-      ->greeting('Dear ' . $this->user->first_name)
-      ->action('', $this->url)
-      ->line(Lang::get("You've requested a password reset for your Geebu  account. Click the link below to set a new password securely:"))
-      ->action(Lang::get('Reset Password'), $this->url)
-      ->line(Lang::get("If you didn't make this request, please contact us immediately at [Support Email or Phone Number]."))
-      ->salutation(new HtmlString($salutation));
-  }
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $salutation = Lang::get("Thank You,<br/>Geebu Support");
+        return (new MailMessage)
+            ->subject(Lang::get('Reset Password Notification'))
+            ->greeting('Dear ' . $this->user->first_name)
+            ->action('Reset Password', $this->url)
+            ->line(Lang::get("You've requested a password reset for your Geebu  account. Click the link below to set a new password securely:"))
+            ->action(Lang::get('Reset Password'), $this->url)
+            ->line(Lang::get("If you didn't make this request, please contact us immediately at [Support Email or Phone Number]."))
+            ->salutation(new HtmlString($salutation));
+    }
 
-  /**
-   * Get the array representation of the notification.
-   *
-   * @return array<string, mixed>
-   */
-  public function toArray(object $notifiable): array
-  {
-    return [
-      //
-    ];
-  }
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
 }
