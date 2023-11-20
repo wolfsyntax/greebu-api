8ad12ce2b7cd109ec75933ee08b242f1d58e6cab
Wolf Syntax <jaysonalpe@gmail.com>
2023-11-20 16:48:03 +0800

    Update for artist song title

--- app/Http/Resources/ArtistResource.php
+++ app/Http/Resources/ArtistResource.php
@@ -87,7 +87,7 @@ class ArtistResource extends JsonResource
             'song_requests'         => $this->song_requests_count ?? 0,
             'genres'                => $this->genres->pluck('genre_title'), //collect($this->genres)->pluck('genre_title'),
             'song'                  => $audio ?? 'https://res.cloudinary.com/daorvtlls/video/upload/v1687411869/merrow-rock-skyline-pigeon-elton-john_h0chm4.mp3',
-            'song'                  => $this->song_title ?? '',
+            'song_title'            => $this->song_title ?? '',
             'cover'                 => $cover ?? '',
             'follower'              => $this->profile->followers_count,
             'following'             => $this->profile->following_count,
