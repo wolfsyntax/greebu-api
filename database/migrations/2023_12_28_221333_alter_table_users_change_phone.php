264836e80e69d80b0ea245c19b33f349f16c0b67
Wolf Syntax <jaysonalpe@gmail.com>
2023-12-28 23:48:51 +0800

    Update for migration

--- database/migrations/2023_12_28_221333_alter_table_users_change_phone.php
+++ database/migrations/2023_12_28_221333_alter_table_users_change_phone.php
@@ -14,7 +14,7 @@ return new class extends Migration
         //
         Schema::table('users', function (Blueprint $table) {
             $table->string('phone')->unique(false)->nullable()->change();
-            $table->dropUnique(['phone']);
+            // $table->dropUnique(['phone']);
         });
     }
 
@@ -24,7 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->nullable()->unique();
+            // $table->string('phone')->nullable()->unique();
         });
     }
 };
