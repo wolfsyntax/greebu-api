8f890ebfd99b0422b75c9fc726fecbbe252cc5ee
Wolf Syntax <jaysonalpe@gmail.com>
2023-12-29 02:08:58 +0800

    Fix for user phone constraint

--- database/migrations/2023_12_29_012638_alter_table_users_change_phone.php
+++ database/migrations/2023_12_29_012638_alter_table_users_change_phone.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->string('phone')->unique(false)->nullable()->change();
+            $table->dropUnique('users_phone_unique');
         });
     }
 
