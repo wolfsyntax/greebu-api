0eb62672f086fa8121757123fcd982f0163cd681
Wolf Syntax <jaysonalpe@gmail.com>
2023-12-29 18:41:58 +0800

    Add seeder for artist category

--- database/seeders/ArtistCategorySeeder.php
+++ database/seeders/ArtistCategorySeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\ArtistCategory;
+
 class ArtistCategorySeeder extends Seeder
 {
     /**
@@ -12,6 +14,9 @@ class ArtistCategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $visualArtists = ArtistCategory::firstOrCreate(['title' => 'Visual Artists',]);
+        $performingArtists = ArtistCategory::firstOrCreate(['title' => 'Performing Artists',]);
+        $literaryArtists = ArtistCategory::firstOrCreate(['title' => 'Literary Artists',]);
+        $digitalArtists = ArtistCategory::firstOrCreate(['title' => 'Digital Artists',]);
     }
 }
