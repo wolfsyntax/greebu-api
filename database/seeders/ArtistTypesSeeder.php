0a29a5059078b393116f8179dfe2f82f98d182c2
Wolf Syntax <jaysonalpe@gmail.com>
2024-01-02 14:13:32 +0800

    Update for seeder

--- database/seeders/ArtistTypesSeeder.php
+++ database/seeders/ArtistTypesSeeder.php
@@ -16,55 +16,55 @@ class ArtistTypesSeeder extends Seeder
     public function run(): void
     {
         //
-         $visual_arts = ['Painter', 'Sculptors', 'Photographers', 'Mural Painter',];
-        $literary_arts = ['Writers', 'Poets',];
-        $digital_arts = ['Graphics Designers', 'Animators', 'Content Writer', 'Copywriters', 'Content creator', ];
-        $performing_arts = ['Disk Jockey', 'Guitarist', 'Vocalist', 'Bassist', 'Drummer', 'Keyboardist', 'Dancers', 'Actors', 'Spoken  Word Artists', 'Host', 'Cosplayer',];
+        //  $visual_arts = ['Painter', 'Sculptors', 'Photographers', 'Mural Painter',];
+        // $literary_arts = ['Writers', 'Poets',];
+        // $digital_arts = ['Graphics Designers', 'Animators', 'Content Writer', 'Copywriters', 'Content creator', ];
+        // $performing_arts = ['Disk Jockey', 'Guitarist', 'Vocalist', 'Bassist', 'Drummer', 'Keyboardist', 'Dancers', 'Actors', 'Spoken  Word Artists', 'Host', 'Cosplayer',];
 
-        $band = ArtistType::where('title', 'Full band')->first();
+        // $band = ArtistType::where('title', 'Full band')->first();
 
-        if ($band) $band->update([
-            'category_id'   => $performingArtists->id,
-            'title' => 'Band',
-        ]);
+        // if ($band) $band->update([
+        //     'category_id'   => $performingArtists->id,
+        //     'title' => 'Band',
+        // ]);
 
-        $solo = ArtistType::where('title', 'Solo Artist')->first();
+        // $solo = ArtistType::where('title', 'Solo Artist')->first();
 
-        if ($solo) $solo->update([
-            'category_id'   => $performingArtists->id,
-        ]);
+        // if ($solo) $solo->update([
+        //     'category_id'   => $performingArtists->id,
+        // ]);
 
-        $visualArtists = ArtistCategory::where('title','Visual Artists')->first();
-        $performingArtists = ArtistCategory::where('title','Performing Artists')->first();
-        $literaryArtists = ArtistCategory::where('title','Literary Artists')->first();
-        $digitalArtists = ArtistCategory::where('title','Digital Artists')->first();
+        // $visualArtists = ArtistCategory::where('title','Visual Artists')->first();
+        // $performingArtists = ArtistCategory::where('title','Performing Artists')->first();
+        // $literaryArtists = ArtistCategory::where('title','Literary Artists')->first();
+        // $digitalArtists = ArtistCategory::where('title','Digital Artists')->first();
 
-        foreach ($visual_arts as $value) {
-            ArtistType::create([
-                'title'         => $value,
-                'category_id'   => $visualArtists->id,
-            ]);
-        }
+        // foreach ($visual_arts as $value) {
+        //     ArtistType::create([
+        //         'title'         => $value,
+        //         'category_id'   => $visualArtists->id,
+        //     ]);
+        // }
 
-        foreach ($literary_arts as $value) {
-            ArtistType::create([
-                'title'         => $value,
-                'category_id'   => $literaryArtists->id,
-            ]);
-        }
+        // foreach ($literary_arts as $value) {
+        //     ArtistType::create([
+        //         'title'         => $value,
+        //         'category_id'   => $literaryArtists->id,
+        //     ]);
+        // }
 
-        foreach ($performing_arts as $value) {
-            ArtistType::create([
-                'title'         => $value,
-                'category_id'   => $performingArtists->id,
-            ]);
-        }
+        // foreach ($performing_arts as $value) {
+        //     ArtistType::create([
+        //         'title'         => $value,
+        //         'category_id'   => $performingArtists->id,
+        //     ]);
+        // }
 
-        foreach ($digital_arts as $value) {
-            ArtistType::create([
-                'title'         => $value,
-                'category_id'   => $digitalArtists->id,
-            ]);
-        }
+        // foreach ($digital_arts as $value) {
+        //     ArtistType::create([
+        //         'title'         => $value,
+        //         'category_id'   => $digitalArtists->id,
+        //     ]);
+        // }
     }
 }
